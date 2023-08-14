8b84a1b071c2660ca30daf48e39cf6a8e4fb0391
Matthew Ferral <mattferral@gmail.com>
2023-08-13 20:37:27 -0400

    updating database connection logic, updating dependencies for database connection

--- backend/config.js
+++ backend/config.js
@@ -13,7 +13,7 @@ function getDatabaseUri() {
 
 const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
 
-console.log("Jobly Config:".green);
+console.log("Deckbuilder Config:".green);
 console.log("SECRET_KEY:".blue, SECRET_KEY);
 console.log("PORT:".blue, PORT.toString());
 console.log("BCRYPT_WORK_FACTOR".blue, BCRYPT_WORK_FACTOR);
--- backend/db.js
+++ backend/db.js
@@ -5,14 +5,14 @@ let db;
 
 if (process.env.NODE_ENV === "production") {
   db = new Client({
-    connectionString: getDatabaseUri(),
+    database: getDatabaseUri(),
     ssl: {
       rejectUnauthorized: false
     }
   });
 } else {
   db = new Client({
-    connectionString: getDatabaseUri(),
+    database: getDatabaseUri(),
   });
 }
 
